c59799d912d30770432a9363aee35b773509a50f
keithAwarren <warrenkeith.music@gmail.com>
2023-04-21 19:37:01 -0230

    commit

---
+++ src/Components/App/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+	      <h1>Sla<span className="highlight">yl</span>ist
+        </h1>
+	        <div className="App">
+		        <div className="App-playlist">		
+            </div>
+	        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
