d68749a0b6e289da4d5057ed0f975fd423e4095c
keithAwarren <warrenkeith.music@gmail.com>
2023-04-22 13:36:15 -0230

    commit

--- src/Components/SearchResults/SearchResults.js
+++ src/Components/SearchResults/SearchResults.js
@@ -1,13 +1,13 @@
 import React from "react";
 import "./SearchResults"
-import Track from "../Track/Track";
+import TrackList from "../TrackList/TrackList";
 
 class SearchResults extends React.Component {
     render () {
         return (
             <div className="SearchResults">
                 <h2>Results</h2>
-                <TrackList /> 
+                <TrackList track/> 
             </div>
         );
     }
