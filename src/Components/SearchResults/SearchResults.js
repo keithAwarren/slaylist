09adf9fc820c6de96f78893a2aef6f2f60d3ca8e
Keith Warren <128069942+keithAwarren@users.noreply.github.com>
2023-05-13 12:25:31 -0230

    Add comments to SearchResults.js

--- src/Components/SearchResults/SearchResults.js
+++ src/Components/SearchResults/SearchResults.js
@@ -1,6 +1,13 @@
 import React from "react";
 import "./SearchResults.css";
+
+// Tracklist is imported to be inserted into the SearchResults component.
+
 import TrackList from "../TrackList/TrackList";
+
+// The SearchResults function
+// The TrackList component receives props that include an array of tracks to be displayed,
+// as well as the function to add tracks to the playlist.
 
 const SearchResults = (props) => {
   return (
