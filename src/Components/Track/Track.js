84796c777c05cd525900ac75b337ba109a9b1418
Keith Warren <128069942+keithAwarren@users.noreply.github.com>
2023-05-13 12:09:32 -0230

    Update Track.js
    Add comments

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -2,12 +2,17 @@ import React, { useCallback } from "react";
 import "./Track.css";
 
 const Track = (props) => {
+  
+  // addTrack calls the onAdd function passed in through props, passing the current track as an argument.
+  
   const addTrack = useCallback(
     (event) => {
       props.onAdd(props.track);
     },
     [props.onAdd, props.track]
   );
+  
+  // removeTrack calls the onRemove function passed in through props, passing the current track as an argument.
 
   const removeTrack = useCallback(
     (event) => {
@@ -15,6 +20,10 @@ const Track = (props) => {
     },
     [props.onRemove, props.track]
   );
+  
+  // This function returns a button element. 
+  // If the isRemoval prop is true, the button will display " - " and calls the removeTrack function when clicked.
+  // Otherwise, the button displays a " + " and calls the addTrack function when clicked.
 
   const renderAction = () => {
     if (props.isRemoval) {
@@ -30,6 +39,9 @@ const Track = (props) => {
         </button>
     );
   };
+
+  // Renders the track with its name, artist name, and album name.
+  // Also renders the button defined in the renderAction function.
 
   return (
     <div className="Track">
